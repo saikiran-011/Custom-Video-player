8c251adccf21caa11221b00b26f0f89d1e21a212
Sai Kiran Budumuru <saikiranb117@gmail.com>
2026-05-17 22:56:43 +0530

    Started JS

---
+++ script.js
@@ -0,0 +1,19 @@
+const video = document.getElementById('video');
+const videoThumbnail = document.getElementById('thumbnail-img');
+
+const playPause = document.getElementById('play-pause');
+const forward = document.getElementById('skip-10');
+const backward = document.getElementById('skip-minus-10');
+const volume = document.getElementById('volume');
+const muteButton = document.getElementById('mute');
+
+
+const videoContainer = document.querySelector('.video-container');
+const controls = document.querySelector('.controls');
+const progressBar = document.querySelector('.progress-bar');
+const playBackLine = document.querySelector('.playback-line');
+
+
+const currentTime = document.getElementById('current-time');
+const maxDuration = document.getElementById('max-duration');
+const fullScreenButton = document.getElementById('fullscreen-btn');
