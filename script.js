6a079f2e29756302fe818020381ac078b749e435
Sai Kiran Budumuru <saikiranb117@gmail.com>
2026-05-19 11:19:46 +0530

    Added some JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const video = document.getElementById('video');
-const videoThumbnail = document.getElementById('thumbnail-img');
+const videoThumbnail = document.getElementById('thumbnail-image');
 
 const playPause = document.getElementById('play-pause');
 const forward = document.getElementById('skip-10');
@@ -16,4 +16,35 @@ const playBackLine = document.querySelector('.playback-line');
 
 const currentTime = document.getElementById('current-time');
 const maxDuration = document.getElementById('max-duration');
-const fullScreenButton = document.getElementById('fullscreen-btn');
+const fullScreenButton = document.querySelector('.fullscreen-btn');
+
+
+playPause.addEventListener('click', togglePlayPause);
+
+video.addEventListener('click', togglePlayPause);
+
+video.addEventListener('play', () => {
+    playPause.innerHTML = '<i class="fa-solid fa-pause"></i>';
+});
+
+video.addEventListener('pause', () => {
+    playPause.innerHTML = '<i class="fa-solid fa-play"></i>';
+});
+
+
+function togglePlayPause(){
+    if(video.paused){
+        videoThumbnail.style.display = "none";
+        video.play();      
+    }
+    else{
+        video.pause();
+    }   
+}
+
+document.addEventListener('keydown', function (event){
+    if(event.code === "Space"){
+        event.preventDefault();
+        togglePlayPause();
+    }
+})
